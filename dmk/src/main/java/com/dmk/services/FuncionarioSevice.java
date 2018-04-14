4d7b586fbf55c8d7869c219af4c3614a9c2253ea
matsmatos <costamatosmatheus@gmail.com>
2018-04-14 18:26:39 -0300

    Update FuncionarioSevice.java
    sdsdsd

--- dmk/src/main/java/com/dmk/services/FuncionarioSevice.java
+++ dmk/src/main/java/com/dmk/services/FuncionarioSevice.java
@@ -1,7 +1,5 @@
 package com.dmk.services;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -9,21 +7,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.dmk.models.Funcionario;
-import com.dmk.models.Produto;
 import com.dmk.repositories.FuncionarioRepository;
 
 @Service
 public class FuncionarioSevice {
-	
+
 	@Autowired
 	FuncionarioRepository funcionarioRepository;
-	
+
 	public Optional<Funcionario> criarFuncionario(Funcionario funcionario) {
-		
+
 		this.funcionarioRepository.save(funcionario);
-		
+
 		return Optional.ofNullable(funcionario);
-		
+
 	}
-	
+
+	public Optional<List<Funcionario>> listaFuncionario() {
+
+		return Optional.of(this.funcionarioRepository.findAll());
+
+	}
 }
